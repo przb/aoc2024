5a282e203c58c8e7f951c4d8249584f34c7dfe5a
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2024-12-01 15:39:20 -0600

    get 2024 setup

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,5 @@ extern crate aoc_runner_derive;
 extern crate crypto;
 
 pub mod day1;
-pub mod day2;
-pub mod day3;
-pub mod day4;
-pub mod day5;
 
-aoc_lib!{ year = 2015 }
+aoc_lib!{ year = 2024 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-extern crate advent_of_code_2015;
+extern crate aoc2024;
 extern crate aoc_runner_derive;
 extern crate aoc_runner;
 
 use aoc_runner_derive::aoc_main;
 
-aoc_main! { lib = advent_of_code_2015 }
+aoc_main! { lib = aoc2024 }
