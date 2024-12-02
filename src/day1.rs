7e6af1823f81e0d073b3d5ee349b82230616b810
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2024-12-01 19:33:38 -0600

    add heapified trial, not any faster

--- src/day1.rs
+++ src/day1.rs
@@ -1,4 +1,35 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 use itertools::Itertools;
+
+#[allow(dead_code)]
+fn custom_parse_h(
+    input: &str,
+    left: &mut BinaryHeap<Reverse<isize>>,
+    rght: &mut BinaryHeap<Reverse<isize>>,
+) {
+    let mut val = 0;
+    input.as_bytes().iter().for_each(|b| match b {
+        b' ' => {
+            if val != 0 {
+                left.push(Reverse(val));
+            }
+            val = 0;
+        }
+        b'\n' => {
+            rght.push(Reverse(val));
+            val = 0;
+        }
+        b'0'..=b'9' => {
+            val *= 10;
+            val += *b as isize - 48;
+        }
+        _ => panic!("im tired of this grampa"),
+    });
+    if val != 0 {
+        rght.push(Reverse(val));
+    }
+}
 
 #[allow(dead_code)]
 fn custom_parse(input: &str, left: &mut Vec<isize>, rght: &mut Vec<isize>) {
@@ -20,6 +51,9 @@ fn custom_parse(input: &str, left: &mut Vec<isize>, rght: &mut Vec<isize>) {
         }
         _ => panic!("im tired of this grampa"),
     });
+    if val != 0 {
+        rght.push(val);
+    }
 }
 
 #[allow(dead_code)]
@@ -39,6 +73,24 @@ fn get_both<'a>(
             rght.push(line.next().unwrap());
         });
     (left.into_iter(), rght.into_iter())
+}
+
+#[aoc(day1, part1, heapified)]
+fn part1_h(input: &str) -> isize {
+    let mut left = BinaryHeap::with_capacity(1000);
+    let mut rght = BinaryHeap::with_capacity(1000);
+    custom_parse_h(input, &mut left, &mut rght);
+
+    let mut sum = 0;
+    while !(left.is_empty()) {
+        let left = left.pop().unwrap().0;
+        let right = rght.pop().unwrap().0;
+        let diff = isize::abs(left - right);
+
+        sum += diff;
+    }
+
+    sum as isize
 }
 
 #[aoc(day1, part1)]
