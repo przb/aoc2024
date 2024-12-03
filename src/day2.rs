6831474bc6246025cf3e4e8e376174f1308c03f6
Ben Przybyszewski <przybyszewski.benjamin@gmail.com>
2024-12-02 18:43:21 -0600

    add tests for day 2

--- src/day2.rs
+++ src/day2.rs
@@ -20,3 +20,46 @@ pub fn part1(input: &str) -> usize {
         })
         .count()
 }
+
+#[aoc(day2, part2)]
+pub fn part2(input: &str) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod test {
+
+    const SAMPLE_INPUT: &str = "7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9";
+    use super::*;
+    use std::fs;
+
+    fn get_input() -> String {
+        let input_path = "input/2024/day2.txt";
+        fs::read_to_string(input_path).unwrap()
+    }
+
+    #[test]
+    fn part1_sample_input() {
+        assert_eq!(part1(SAMPLE_INPUT), 2)
+    }
+
+    #[test]
+    fn part2_sample_input() {
+        assert_eq!(part2(SAMPLE_INPUT), 4)
+    }
+
+    #[test]
+    fn part1_real_input() {
+        assert_eq!(part1(&get_input()), 463)
+    }
+
+    #[test]
+    fn part2_real_input() {
+        assert_eq!(part2(&get_input()), 0)
+    }
+}
